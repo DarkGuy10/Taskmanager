8eeaf80f05df95dc6b7f0b3a09aaadbf08fc132b
DarkGuy10 <HarperBurn@protonmail.ch>
2023-06-25 22:41:35 +0530

    wip

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import Information from '@/components/Information/Information'
 import SideNav from '@/components/SideNav/SideNav'
 import StickyHeader from '@/components/StickyHeader/StickyHeader'
 import styles from '@/styles/dashboard.module.scss'
+import Image from 'next/image'
 
 export default function Dashboard() {
 	return (
@@ -12,6 +13,62 @@ export default function Dashboard() {
 				<div className={styles.content}>
 					<main className={styles.main}>
 						<Information />
+						<div>
+							<h1 className={styles.header}>
+								All My Tasks
+								<Image
+									src={'/svg/pencil.svg'}
+									alt="pencil-icon"
+									style={{ marginLeft: '10px' }}
+									height={24}
+									width={24}
+								/>
+							</h1>
+							<div className={styles.subHeader}>
+								Managing your tasks is easy with Taskmanager
+							</div>
+							<div className={styles.taskContainer}>
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+								<br />
+							</div>
+						</div>
 					</main>
 					<aside className={styles.aside}></aside>
 				</div>
