5d9a51951de350ff27b9208522a4a4ff7a2904f8
DarkGuy10 <HarperBurn@protonmail.ch>
2023-06-27 11:39:09 +0530

    refactor: add placeholder database type

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,11 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
-// import type {Database} from "@/lib/database.types"
-
-/**
- * Todo: generate typescript types for database using supabase cli
- */
-type DataBase = any
+import type { Database } from '@/lib/database.types'
 
 export default async function middleware(req: NextRequest) {
 	const res = NextResponse.next()
-	const supabase = createMiddlewareClient<DataBase>({ req, res })
+	const supabase = createMiddlewareClient<Database>({ req, res })
 	await supabase.auth.getSession()
 	return res
 }
