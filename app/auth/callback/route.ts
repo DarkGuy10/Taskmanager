990008552d1f3be6ee686ac01213f12ab0341155
DarkGuy10 <HarperBurn@protonmail.ch>
2023-06-28 00:40:46 +0530

    fix: remove default export

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import type { Database } from '@/lib/database.types'
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
 	const requestUrl = new URL(request.url)
 	const code = requestUrl.searchParams.get('code')
 
