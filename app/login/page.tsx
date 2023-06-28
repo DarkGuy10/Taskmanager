f4c41f9a512812b6945ad2c0d20d3238c8645b0d
DarkGuy10 <HarperBurn@protonmail.ch>
2023-06-28 20:47:39 +0530

    feat: add frontend for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,41 @@
-import styles from '@/styles/home.module.scss'
+import Button from '@/components/Button/Button'
+import InputField from '@/components/InputField/InputField'
+import styles from '@/styles/login.module.scss'
+import Image from 'next/image'
 
 export default function Login() {
 	return (
-		<main className={styles.comingSoon}>
-			Login page | <span>in construction </span>
-		</main>
+		<div className={styles.modal}>
+			<div className={styles.header}>Login</div>
+			<div className={styles.subHeader}>
+				Sign in to your account to get continue
+			</div>
+
+			<div className={styles.fieldWrapper}>
+				<InputField
+					type="email"
+					iconUrl="/svg/at-the-rate.svg"
+					label="Email address"
+					placeholder="InumakiToge@jjk.jp"
+				/>
+			</div>
+
+			<div className={styles.fieldWrapper}>
+				<InputField type="password" iconUrl="/svg/key.svg" label="Password" />{' '}
+			</div>
+
+			<div className={styles.forgotPassword}>Forgot password?</div>
+
+			<Button>
+				Sign In{' '}
+				<Image
+					src={'/svg/signin.svg'}
+					height={24}
+					width={24}
+					alt="signin-icon"
+					style={{ marginLeft: '10px' }}
+				/>
+			</Button>
+		</div>
 	)
 }
